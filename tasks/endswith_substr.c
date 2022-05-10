9d01813f2de3c63e80e720504bdebd9856ad1dc6
mposhala <104495822+mposhala@users.noreply.github.com>
2022-05-10 12:41:48 +0530

    Add files via upload

---
+++ tasks/endswith_substr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#define true 1
+#define false 0
+int startswith(char *str, char *substr)
+{
+    int str_len = strlen(str);
+    int substr_len = strlen(substr);
+    for (int i = 0; i < substr_len; i++)
+    {
+        if (str[i+str_len-substr_len] != substr[i])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    char str[] = "General Knowledge";
+    char substr[] = "Knowledge";
+    if (startswith(str, substr))
+        printf("yes, string ends with substring.\n");
+    else
+        printf("No, string  not ends with substring.\n");
+    return 0;
+}
